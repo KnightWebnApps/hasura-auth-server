68220773f9c88134427c05b21a7e4416e983437d
MyMac <mymac@MacBook-Pro-8.local>
2020-09-30 18:16:22 -0400

    debugging me query

--- resolvers.js
+++ resolvers.js
@@ -56,8 +56,7 @@ const signOptions = {
 const resolvers = {
   Query: {
     me: async (_, args, req) => {
-      console.log(req.rawHeaders[15])
-      if (req.userId) {
+      if (req.rawHeaders[15] !== undefined) {
         const user = await graphql.request(ME, { id: req.userId }).then(data => {
           return data.user[0]
         })
